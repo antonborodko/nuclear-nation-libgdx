6030e566c5400749a84900c4c7f5781c16b21d99
Anton Borodko <antonborodko@gmail.com>
2019-01-15 19:10:25 +0200

    Added create command button #f7655

--- core/src/com/anton/nuclearnation/UnitConstructionScreen.scala
+++ core/src/com/anton/nuclearnation/UnitConstructionScreen.scala
@@ -1,9 +1,9 @@
 package com.anton.nuclearnation
 
-import com.badlogic.gdx.{Gdx, Screen}
+import com.badlogic.gdx.{Gdx, Input, InputProcessor, Screen}
 import com.badlogic.gdx.graphics.{Color, GL20, OrthographicCamera}
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.{Label, Skin}
+import com.badlogic.gdx.scenes.scene2d.ui.{Button, Label, Skin, TextButton}
 import com.badlogic.gdx.utils.viewport.StretchViewport
 
 class UnitConstructionScreen(game:NuclearNation,mapScreen: MapScreen) extends Screen{
@@ -16,10 +16,42 @@ class UnitConstructionScreen(game:NuclearNation,mapScreen: MapScreen) extends Sc
 
   val skin = assetManager.get("data/commodore64/skin/uiskin.json",classOf[Skin])
 
+
+  val constructionScreenInputProcessor = new InputProcessor() {
+
+    override def keyUp(keycode: Int): Boolean = {
+      if (keycode == Input.Keys.ESCAPE || keycode == Input.Keys.T) {
+        game.setScreen(mapScreen)
+        return true
+      }
+      false
+    }
+
+    override def keyDown(keycode: Int): Boolean = {return true}
+
+    override def keyTyped(character: Char): Boolean = {return true}
+
+    override def touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {return true}
+
+    override def touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = {return true}
+
+    override def touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = {return true}
+
+    override def mouseMoved(screenX: Int, screenY: Int): Boolean = {return true}
+
+    override def scrolled(amount: Int): Boolean = {return true}
+  }
+
+  Gdx.input.setInputProcessor(constructionScreenInputProcessor)
+
   override def show(): Unit = {
     val titleLabel = new Label("Unit construction",skin)
-    titleLabel.setPosition(stage.getViewport.getScreenWidth/2,stage.getViewport.getScreenHeight)
+    val commandoUnitButton = new TextButton("Commando",skin)
+    commandoUnitButton.setDisabled(mapScreen.technologies.count(t => !t.enabled)>0)
+    titleLabel.setPosition(stage.getViewport.getScreenWidth/2,stage.getViewport.getScreenHeight-20)
+    commandoUnitButton.setPosition(stage.getViewport.getScreenWidth/2,stage.getViewport.getScreenHeight-100)
     stage.addActor(titleLabel)
+    stage.addActor(commandoUnitButton)
 
   }
 
