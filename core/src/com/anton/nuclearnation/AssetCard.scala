2bbc06c1013ea01b1d68a2b5bfc3521843c942cf
Anton Borodko <anton.borodko@ridewithvia.com>
2019-02-03 17:45:08 +0200

    Working on generic asset card class #expdd[in progress]

---
+++ core/src/com/anton/nuclearnation/AssetCard.scala
@@ -0,0 +1,22 @@
+package com.anton.nuclearnation
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.{Batch, TextureRegion}
+import com.badlogic.gdx.scenes.scene2d.ui.{Image, Label, Skin, Table}
+import com.badlogic.gdx.scenes.scene2d.{Actor, Group}
+
+class AssetCard(texture:Texture,count:Int,game:NuclearNation) extends Group{
+
+
+  val table = new Table
+
+  val assetManager = game.assetManager
+  val skin = assetManager.get("data/commodore64/skin/uiskin.json",classOf[Skin])
+
+  val countLabel = new Label(count.toString,skin)
+
+  table.add(new Image(texture)).center()
+  table.row()
+  table.add(countLabel).right().bottom()
+
+}
