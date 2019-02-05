636a9f22e2dd0c06c241a42c498248cc5fa4f1b2
Anton Borodko <anton.borodko@ridewithvia.com>
2019-02-05 17:32:13 +0200

    Changed map screen music #exp9j[closed]

--- core/src/com/anton/nuclearnation/MapScreen.scala
+++ core/src/com/anton/nuclearnation/MapScreen.scala
@@ -101,7 +101,7 @@ class MapScreen(game: NuclearNation) extends Screen{
 
   var isPaused = false
 
-  val music = Gdx.audio.newMusic(Gdx.files.internal("music/POL-dark-crossing-short.mp3"))
+  val music = Gdx.audio.newMusic(Gdx.files.internal("music/POL-torn-short.mp3"))
 
   val randomCaravanSpawnChance =  sys.env.get("RANDOM_CARAVAN_SPAWN_CHANCE") match {
     case Some(v)=>v.toLowerCase().toInt
