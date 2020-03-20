b54d8fecbef527146fecdb9274c2a3b020d76c97
Anton Borodko <antonborodko@gmail.com>
2020-03-21 00:30:22 +0200

    Scalatest is working now

--- core/src/test/scala/com/anton/nuclearnation/NationLevelSuite.scala
+++ core/src/test/scala/com/anton/nuclearnation/NationLevelSuite.scala
@@ -1,14 +1,11 @@
 package com.anton.nuclearnation
 
-import org.junit.runner.RunWith
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatestplus.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
 class NationLevelSuite extends AnyFunSuite {
 
   test("An empty Set should have size 0") {
-    assert(Set.empty.size == 1)
+    assert(Set.empty.size == 0)
   }
 
   test("Invoking head on an empty Set should produce NoSuchElementException") {
